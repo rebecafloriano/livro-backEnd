21b553fd5add99d959dea5e32b690226d7348d04
nomeRebeca <rebecaoliver09@gmail.com>
2023-12-28 20:18:08 +0000

    testanto

--- demo/src/main/java/com/example/app/controller/Teste.java
+++ demo/src/main/java/com/example/app/controller/Teste.java
@@ -15,7 +15,7 @@ public class Teste {
         try {
             // Teste uma consulta simples ao banco de dados
             jdbcTemplate.queryForObject("SELECT 1", Integer.class);
-            return "Conexão com o banco de dados bem-sucedida!";
+            return "Conexão com o banco de dados bem-sucedida :) !";
         } catch (Exception e) {
             // Se ocorrer uma exceção, algo está errado
             return "Erro na conexão com o banco de dados: " + e.getMessage();
